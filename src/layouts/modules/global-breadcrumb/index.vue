3a936190b874206f4df1211bad5482f8f45a585d
qi yuhang <qiyuhang@thinkerx.com>
2023-12-01 20:13:20 +0800

    refactor(projects): change breadcrumb to element-plus

--- src/layouts/modules/global-breadcrumb/index.vue
+++ src/layouts/modules/global-breadcrumb/index.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { createReusableTemplate } from '@vueuse/core';
 import type { RouteKey } from '@elegant-router/types';
 import { useThemeStore } from '@/store/modules/theme';
 import { useRouteStore } from '@/store/modules/route';
 import { useRouterPush } from '@/hooks/common/router';
-
+import { ArrowRight } from '@element-plus/icons-vue';
+import { ElDropdown } from 'element-plus';
 defineOptions({
   name: 'GlobalBreadcrumb'
 });
@@ -13,35 +13,31 @@ const themeStore = useThemeStore();
 const routeStore = useRouteStore();
 const { routerPushByKey } = useRouterPush();
 
-interface BreadcrumbContentProps {
-  breadcrumb: App.Global.Menu;
-}
-
-const [DefineBreadcrumbContent, BreadcrumbContent] = createReusableTemplate<BreadcrumbContentProps>();
-
 function handleClickMenu(key: RouteKey) {
   routerPushByKey(key);
 }
 </script>
 
 <template>
-  <NBreadcrumb v-if="themeStore.header.breadcrumb.visible">
-    <!-- define component: BreadcrumbContent -->
-    <DefineBreadcrumbContent v-slot="{ breadcrumb }">
-      <div class="i-flex-y-center align-middle">
-        <component :is="breadcrumb.icon" v-if="themeStore.header.breadcrumb.showIcon" class="mr-4px text-icon" />
-        {{ breadcrumb.label }}
-      </div>
-    </DefineBreadcrumbContent>
-    <!-- define component: BreadcrumbContent -->
-
-    <NBreadcrumbItem v-for="item in routeStore.breadcrumbs" :key="item.key">
-      <NDropdown v-if="item.options?.length" :options="item.options" @select="handleClickMenu">
-        <BreadcrumbContent :breadcrumb="item" />
-      </NDropdown>
-      <BreadcrumbContent v-else :breadcrumb="item" />
-    </NBreadcrumbItem>
-  </NBreadcrumb>
+  <ElBreadcrumb v-if="themeStore.header.breadcrumb.visible" :separator-icon="ArrowRight">
+    <ElBreadcrumbItem v-for="item in routeStore.breadcrumbs" :key="item.key">
+      <ElDropdown v-if="item.options?.length">
+        <ElButton text :icon="themeStore.header.breadcrumb.showIcon ? item.icon : ''">
+          {{ item.label }}
+        </ElButton>
+        <template #dropdown>
+          <ElDropdownMenu>
+            <ElDropdownItem v-for="option in item.options" :key="option.key" @click="handleClickMenu(option.routeKey)">
+              {{ option.label }}
+            </ElDropdownItem>
+          </ElDropdownMenu>
+        </template>
+      </ElDropdown>
+      <ElButton v-else text bg :icon="themeStore.header.breadcrumb.showIcon ? item.icon : ''" class="!cursor-default">
+        {{ item.label }}
+      </ElButton>
+    </ElBreadcrumbItem>
+  </ElBreadcrumb>
 </template>
 
 <style scoped></style>
